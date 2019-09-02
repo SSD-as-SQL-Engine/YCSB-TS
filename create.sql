e8a8945c87b916b293767c9f98d0a89d97995ad3
Csoyee <ithdli@naver.com>
2019-09-02 17:35:34 +0900

    Create create.sql

---
+++ create.sql
@@ -0,0 +1,15 @@
+CREATE TABLE "usermetric" (
+    	"ID"	INTEGER PRIMARY KEY AUTOINCREMENT,
+    	"YCSB_KEY"	TIMESTAMP,
+    	"VALUE"	DOUBLE,
+    	"TAG0"	TEXT,
+    	"TAG1"	TEXT,
+    	"TAG2"	TEXT,
+    	"TAG3"	TEXT,
+    	"TAG4"	TEXT,
+    	"TAG5"	TEXT,
+    	"TAG6"	TEXT,
+    	"TAG7"	TEXT,
+    	"TAG8"	TEXT,
+    	"TAG9"	TEXT
+);
